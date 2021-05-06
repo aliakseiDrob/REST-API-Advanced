7b3dca335f99499ff34c2c2327c3ec9809c6d006
Aleksei Drob <d1061606@yandex.ru>
2021-05-06 23:34:34 +0300

    orderDaoTest

--- persistence/src/test/java/com/epam/esm/dao/impl/OrderDaoImplTest.java
+++ persistence/src/test/java/com/epam/esm/dao/impl/OrderDaoImplTest.java
@@ -1,9 +1,7 @@
 package com.epam.esm.dao.impl;
 
 import com.epam.esm.config.TestConfig;
-import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.OrderDao;
-import com.epam.esm.dao.UserDao;
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.entity.Order;
 import com.epam.esm.entity.User;
@@ -50,9 +48,25 @@ public class OrderDaoImplTest {
         //then
         assertEquals(expectedOrder, orderDao.getUserOrder(1L, 1L));
     }
+
     @Test
     public void testGetRowsCountShouldReturnAmountRowsInDb() {
         //then
-        assertEquals(3L,orderDao.getRowsCount());
+        assertEquals(3L, orderDao.getRowsCount());
+    }
+
+    @Test
+    @Rollback
+    public void testCreateOrderShouldCreateOrder() {
+        //given
+        User user = new User(1L, "Ivan");
+        Order order = new Order(0, LocalDateTime.parse("2021-03-24 20:11:10", DATE_TIME_FORMATTER),
+                new BigDecimal("340.00"), user);
+        GiftCertificate giftCertificate = new GiftCertificate(1L, "first", "for men",
+                new BigDecimal("128.01"), 11, 1, LocalDateTime.now(), LocalDateTime.now());
+        //then
+        order.setCertificate(giftCertificate);
+        assertEquals(order, orderDao.createOrder(order));
+
     }
 }
